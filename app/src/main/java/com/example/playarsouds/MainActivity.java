84840ac7b88c7061e2b5ffc6cb5f115913436a16
Rafael Fontana Becker Gois <joefontanabg@gmail.com>
2022-09-23 09:25:35 -0300

    initial commit

--- app/src/main/java/com/example/playarsouds/MainActivity.java
+++ app/src/main/java/com/example/playarsouds/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.playarsouds;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaParser;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
+
+    private SeekBar seekBarVolume;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,73 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.bach);
+
+        inicializarSeekBar();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    private void inicializarSeekBar () {
+        seekBarVolume = findViewById(R.id.seekBar);
+
+        //configurando audio
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        //pega o volume maximo das musicas
+        int volMax = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int volAtual = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        //configurando o vol maximo do seak bar
+
+        seekBarVolume.setMax(volMax);
+
+        //atual
+        seekBarVolume.setProgress(volAtual);
+
+        seekBarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,i,0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
     public void executeSound (View view) {
